3c6c68ebf0780337e7f0234e67185e5586d7c836
caio180 <caio.lima.viana2009@gmail.com>
2025-08-25 17:08:20 +0000

    Melhoria

--- Conversor de moeda.py
+++ Conversor de moeda.py
@@ -1,32 +1,32 @@
 print(" Dólar dos Estados Unidos USD\n Libra de Gibraltar GIP\n Libra Esterlina GBP\n Peso Chileno CLP\n O Rupia do Sri Lanka LKR\n Euro EUR\n Bitcoin BTC, \n Em breve terá 3 novas moedas." )
-operador = input("Digite a moeda de seu desejo: ")
+moeda = input("Digite a moeda de seu desejo: ")
 reais = int(input("Qual o seu valor em reais ? "))
 
-if operador == "USD" or operador == "usd":
+if moeda == "USD" or moeda == "usd":
    resultado = (reais * 0.1762487)
    print(f"O resultado da conversão é: {resultado}")
 
-elif operador == "GIP" or operador == "gip":
+elif moeda == "GIP" or moeda == "gip":
    resultado = (reais * 0.1327175)
    print (f"O resultado da conversão é: {resultado}")
 
-elif operador == "GBP" or operador == "gbp":
+elif moeda == "GBP" or moeda == "gbp":
    resultado = (reais * 0.1324486)
    print(f"O resultado da conversão é: {resultado}")
 
-elif operador == "CLP" or operador == "clp":
+elif moeda == "CLP" or moeda == "clp":
    resultado = (reais * 165.5629139)
    print(f"O resultado da conversão é: {resultado}")
    
-elif operador == "LKR" or operador == "lkr":
+elif moeda == "LKR" or moeda == "lkr":
    resultado = (reais * 52.8541226)
    print(f"O resultado da conversão é: {resultado}")
 
-elif operador == "EUR" or operador == "eur":
+elif moeda == "EUR" or moeda == "eur":
    resultado = (reais * 0.155094)
    print (f"O resultado da conversão é: {resultado}")
 
-elif operador == "BTC" or operador == "btc":
+elif moeda == "BTC" or moeda == "btc":
    resultado = (reais * 0.0000018)
    print(f"O resultado da conversão é: {resultado}")
 
