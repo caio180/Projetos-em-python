1de62ec1d14f1d4e59bdcfe971ad1bea862a3c1d
caio180 <caio.lima.viana2009@gmail.com>
2025-08-27 22:44:12 +0000

    Novo

--- Projeto.py
+++ Projeto.py
@@ -1,8 +1,17 @@
-def verificar_par(numero):
-  if numero % 2 == 0:
-   return True
-  else: 
-   return False
-   
-resultado = verificar_par(6)
-print(resultado)
+def sugestões_de_como_ser_um_bom_programador():
+   print("Algumas sugestões de como ser um bom programador: ")
+   sugestoes = [
+        "1. Pratique regularmente: A prática constante é essencial para aprimorar suas habilidades de programação.",
+        "2. Estude algoritmos e estruturas de dados: Compreender esses conceitos fundamentais ajudará a escrever código mais eficiente.",
+        "3. Leia código de outros programadores: Analisar o código de outros pode fornecer novas perspectivas e técnicas.",
+        "4. Participe de projetos open source: Contribuir para projetos open source pode melhorar suas habilidades e expandir sua rede profissional.",
+        "5. Mantenha-se atualizado com as tendências tecnológicas: A tecnologia está em constante evolução, então é importante estar sempre aprendendo.",
+        "6. Escreva código limpo e documentado: Código bem escrito e documentado facilita a manutenção e colaboração com outros desenvolvedores.",
+        "7. Teste seu código: Implementar testes ajuda a garantir que seu código funcione conforme o esperado e reduz bugs.",
+        "8. Aprenda novas linguagens de programação: Diversificar seu conhecimento em diferentes linguagens pode abrir novas oportunidades.",
+        "9. Participe de comunidades de programação: Engajar-se com outras pessoas na área pode proporcionar suporte, feedback e aprendizado contínuo.",
+        "10. Desenvolva habilidades de resolução de problemas: Ser capaz de abordar e resolver problemas complexos é uma habilidade crucial para qualquer programador.",
+        "11. Aprenda uma linguagem por vez, se você tentar aprender várias de uma vez pode acabar se confundindo" ] 
+   for sugestao in sugestoes:
+        print(sugestao)
+sugestões_de_como_ser_um_bom_programador()
