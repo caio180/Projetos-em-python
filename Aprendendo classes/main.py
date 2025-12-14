61c5ef8b3271183f488b413f1042993295c35fa9
caio180 <caio.lima.viana2009@gmail.com>
2025-12-14 18:59:20 -0300

    Novo

---
+++ Aprendendo classes/main.py
@@ -0,0 +1,15 @@
+class Vendedor:
+    def __init__(self):
+        self.nome = input("Digite o nome do vendedor: ")
+        self.vendas = 0
+    
+    def metas(self):
+        self.vendas = int(input("Digite a suas vendas totais: "))
+
+        if self.vendas >= 700:
+            print(f"{self.nome}, atingiu a meta de vendas.")
+        else:
+            print(f"{self.nome}, não atingiu a meta.")
+
+vendedor1 = Vendedor()
+vendedor1.metas()
