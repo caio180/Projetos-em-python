fb50140a0bcb9c72faa3dd6444c02e8f77e7fc00
caio180 <caio.lima.viana2009@gmail.com>
2025-12-17 14:13:45 -0300

    Atualização

--- Conversor de temperatura.py
+++ Conversor de temperatura.py
@@ -1,27 +1,27 @@
 print("Conversor de temperatura: \n1: temperatura kelvin para Celsius \n2: temperatura Celsius para kelvin \n3: temperatura Fahrenheit para Celsios \n4: temperatura Celsius para Fahrenheit \n5: temperatura kelvin para Fahrenheit \n6: temperatura Fahrenheit para kelvin")
-Escala = input(" Escolha o tipo de escala Termométrica = ")
+escala = input(" Escolha o tipo de escala Termométrica = ").strip().lower()
 
-if Escala == "kelvin para Celsius" or Escala == "Kelvin para Celsius" or Escala == "K para C" or Escala == "k para c" or Escala == "KpC" or Escala == "kpc" or Escala == "1":
+if escala.lower() in("kelvin para celsius", "k para c", "1"):
    tk = float(input("K = "))
    temperatura_kelvin_p_Celsius = tk - 273.15
-   print(round(temperatura_kelvin_p_Celsius), "°C.")
-elif Escala == "Celsius para kelvin" or Escala == "celsius para kelvin" or Escala == "C para K" or Escala == "c para k" or Escala == "CpK" or Escala == "cpk" or Escala == "2":
+   print(round(temperatura_kelvin_p_Celsius, 2), "°C.")
+elif escala.lower()  in("celsius para kelvin", "c para k", "2"):
     tc = float(input("C = "))
     temperatura_Celsius_p_kelvin = tc + 273.15
-    print(round(temperatura_Celsius_p_kelvin), "°K.")
-elif Escala == "Fahrenheit para Celsius" or Escala == "fahrenheit para celsius" or Escala == "F para C" or Escala == "f para c" or Escala == "FpC" or Escala == "fpc" or Escala == "3":
+    print(round(temperatura_Celsius_p_kelvin, 2), "°K.")
+elif escala.lower() in("fahrenheit para celsius", "f para c", "3"):
     tf = float(input("F = "))
     temp_Fahrenheit_p_C = (tf-32) * 5/9
-    print(round(temp_Fahrenheit_p_C), "°C.")
-elif Escala == "Celsius para Fahrenheit" or Escala == "celsius para fahrenheit" or Escala == "C para F" or Escala == "c para f" or Escala == "CpF" or Escala == "cpf" or Escala == "4":
+    print(round(temp_Fahrenheit_p_C, 2), "°C.")
+elif escala.lower() in("celsius para fahrenheit", "c para f", "4"):
     tcf = float(input("Cf = "))
     tempcf = (tcf * 9/5 ) + 32
-    print(round(tempcf), " °F.")
-elif Escala == "kelvin para Fahrenheit" or Escala == "Kelvin para Fahrenheit" or Escala == "K para F" or Escala == "k para f" or Escala == "KpF" or Escala == "kpf" or Escala == "5":
+    print(round(tempcf, 2), " °F.")
+elif escala.lower() in("kelvin para fahrenheit", "k para f", "5"):
     tkf = float(input("Kf = "))
     tempkf = (tkf-273.15) * 9/5 + 32
-    print(round(tempkf), "°F.")
-elif Escala == "Fahrenheit para kelvin" or Escala == "fahrenheit para kelvin" or Escala == "F para K" or Escala == "f para k" or Escala == "Fpk" or Escala == "fpk" or Escala == "6":
+    print(round(tempkf, 2), "°F.")
+elif escala.lower() in("fahrenheit para kelvin", "f para k", "Fpk", "6"):
     tfk = float(input("Fk = "))
     tempfk = (tfk-32) * 5/9 + 273.15 
-    print(round(tempfk), "°K.")
+    print(round(tempfk, 2), "°K.")
