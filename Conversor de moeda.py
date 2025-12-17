e567083459bc13b69b7e945c958270ed4e6d71c0
caio180 <caio.lima.viana2009@gmail.com>
2025-12-17 14:22:24 -0300

    Atualização

--- Conversor de moeda.py
+++ Conversor de moeda.py
@@ -2,31 +2,31 @@ print(" Dólar dos Estados Unidos USD\n Libra de Gibraltar GIP\n Libra Esterlina
 moeda = input("Digite a moeda de seu desejo: ")
 reais = int(input("Qual o seu valor em reais ? "))
 
-if moeda == "USD" or moeda == "usd":
+if moeda.lower() in ("1", "usd"):
    resultado = (reais * 0.1762487)
    print(f"O resultado da conversão é: {resultado}")
 
-elif moeda == "GIP" or moeda == "gip":
+elif moeda.lower() in ("2", "gip"):
    resultado = (reais * 0.1327175)
    print (f"O resultado da conversão é: {resultado}")
 
-elif moeda == "GBP" or moeda == "gbp":
+elif moeda.lower() in ("3", "gbp"):
    resultado = (reais * 0.1324486)
    print(f"O resultado da conversão é: {resultado}")
 
-elif moeda == "CLP" or moeda == "clp":
+elif moeda.lower() in ("1", "clp"):
    resultado = (reais * 165.5629139)
    print(f"O resultado da conversão é: {resultado}")
    
-elif moeda == "LKR" or moeda == "lkr":
+elif moeda.lower() in ("5", "lkr"):
    resultado = (reais * 52.8541226)
    print(f"O resultado da conversão é: {resultado}")
 
-elif moeda == "EUR" or moeda == "eur":
+elif moeda.lower() in ("6", "eur"):
    resultado = (reais * 0.155094)
    print (f"O resultado da conversão é: {resultado}")
 
-elif moeda == "BTC" or moeda == "btc":
+elif moeda.lower() in ("7", "btc"):
    resultado = (reais * 0.0000018)
    print(f"O resultado da conversão é: {resultado}")
 
