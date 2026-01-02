eecd1e898e8b2710a4707ef3f708de18888ca173
caio180 <caio.lima.viana2009@gmail.com>
2026-01-02 14:18:44 -0300

    Aprimorando código

--- Manipu_arquivos/Sistema de cadrasto.py
+++ Manipu_arquivos/Sistema de cadrasto.py
@@ -1,13 +1,13 @@
 opcoes = input("Digite uma dessas opções: \n1)Criar uma conta \n2) Já tenho uma conta \n ")
 
-if opcoes in ("1", "Criar uma conta", "criar uma conta", "criar conta"):
+if opcoes.lower() in ("1", "criar uma conta", "criar conta"):
     with open("cadrastos.txt", "a") as cadastro:
          criar_email = input("Digite o nome do email que deseja, exemplo (cleito.silva@gmail.com): ")
          criar_senha = input("Digite a senha que deseja, (lembre-se: Sua senha tem que ser forte): ")
          cadastro.write("Email cadastrado: " + criar_email + "\n")
          cadastro.write("Senha: " + criar_senha + "\n\n") 
 
-elif opcoes in ("2", "Já tenho uma conta", "Já tenho"):
+elif opcoes.lower() in ("2", "já tenho uma conta", "já tenho", "ja tenho"):
          try:
             with open("cadastros.txt", "r", encoding="utf-8") as cadastrados:
                 print(cadastrados.read())
