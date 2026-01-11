fe78c47240ed784582d7e23c9452beaa4558ff99
caio180 <caio.lima.viana2009@gmail.com>
2026-01-11 13:40:59 -0300

    Atualização na forma de login e de alterar senha

--- Projetos_python_Sql/Sistema_de_cadastro/main.py
+++ Projetos_python_Sql/Sistema_de_cadastro/main.py
@@ -11,8 +11,11 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS usuarios (
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
     email TEXT UNIQUE NOT NULL,
-    senha TEXT NOT NULL        
-                )""")
+    senha BLOB NOT NULL
+)
+""")
+
+
 
 opcoes = input("Digite a opção desejada: \n1) Criar uma conta \n2) Fazer login \n3) Alterar senha \n= ")
 
@@ -37,44 +40,45 @@ if opcoes.lower() in ("1", "criar conta"):
 elif opcoes.lower() in ("2", "fazer login"):
     email = input("Digite seu email: ")
     senha = input("Digite sua senha: ")
-    senha_hash = gerar_hash(senha)
-
-    cursor.execute("""  
-        SELECT * FROM usuarios WHERE email = ? AND senha = ?
-                   
-                """, (email, senha_hash))
     
-    if cursor.fetchone():
+    cursor.execute("""
+        SELECT senha FROM usuarios WHERE email = ?
+    """, (email,))
+
+    resultado = cursor.fetchone()
+
+    if resultado and bcrypt.checkpw(senha.encode(), resultado[0]):
         print("Login realizado com sucesso!!")
     else:
-        print("ERRO: email ou senha incorretos.") 
+        print("ERRO: email ou senha incorretos.")
+
 
 elif opcoes.lower() in ("3", "alterar senha"):
     email = input("Digite seu email: ")
     senha_atual = input("Digite sua senha atual: ")
-
-    senha_atual_hash = gerar_hash(senha_atual)
-
-    cursor.execute("""     
-        SELECT * FROM usuarios WHERE email = ? AND senha = ?
-                   
-                """, (email, senha_atual_hash))
     
-    if cursor.fetchone():
+    cursor.execute("""
+        SELECT senha FROM usuarios WHERE email = ?
+    """, (email,))
+
+    resultado = cursor.fetchone()
+
+    if resultado and bcrypt.checkpw(senha_atual.encode(), resultado[0]):
         nova_senha = input("Digite uma nova senha: ")
+
         if len(nova_senha) < 8:
             print("Erro: a senha precisa de no mínimo 8 caracteres.")
         else:
             nova_senha_hash = gerar_hash(nova_senha)
             cursor.execute("""
-            UPDATE usuarios
-            SET senha = ?
-            WHERE email = ?
-                                """, (nova_senha_hash, email))
+                UPDATE usuarios
+                SET senha = ?
+                WHERE email = ?
+            """, (nova_senha_hash, email))
             banco.commit()
-            print("Senha alterada com sucesso! ")
+            print("Senha alterada com sucesso!")
     else:
-        print("Email ou senha incorretos, tente novamente.")
+        print("Email ou senha incorretos.")
 
 cursor.close()
 banco.close()     
